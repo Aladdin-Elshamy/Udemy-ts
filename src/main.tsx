ead137cb9640da6d27ed694bb62b207282e054c0
sayedosama7 <sayedosama088@gmail.com>
2024-09-20 03:03:09 +0300

    courses and customer stories and testimonials

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './index.css';
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+]);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+	createRoot(rootElement).render(<RouterProvider router={router} />);
+}
