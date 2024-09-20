f5c5768e09684ec7b2c61532d9dc9fb1f2fd04e2
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-09-20 17:31:04 +0300

    ImageSlider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
-
+import ImageSlider from "./components/ImageSlider";
+import { images } from "./data";
 function App() {
   return (
     <>
       <header>
         <Navbar />
+        <div className="lg:container h-96 pt-0 ">
+          <ImageSlider images={images} />
+        </div>
       </header>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container:{
+      padding:{
+        DEFAULT:'1rem',
+        sm:'2rem',
+        lg:'4rem',
+        xl:'5rem',
+        '2xl':'6rem',
+      },
+      center:true
+    },
     extend: {
       colors: {
         'hover': '#9d42d7',
