e40aa9c823ee13f0862e6eb7cb6605c9a5739613
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2024-09-21 20:15:19 +0300

    Parteners section

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,6 +1,6 @@
 export default function Partners() {
   return (
-    <section className="bg-gray-100 py-4 md:mt-16">
+    <section className="bg-gray-100 py-14 md:mt-16">
       <div className="container">
         <p className="text-gray-500 text-lg pb-5 text-center">
           Trusted by over 16,000 companies and millions of learners around the
